48498b8d3497af3b44f0d5d905da1affb854a541
Ofallbrains <ofallbrains@gmail.com>
2023-11-16 01:50:39 -0800

    stack and queues

---
+++ fifo.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+  * fifo - changes the addition to the stack to FIFO
+  * @stack: NULL
+  * @line_number: 0
+  * Return: nothing, void
+  */
+void fifo(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+
+	element->IFO = 1;
+}
---
+++ lifo.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+  * lifo - changes the addition to the stack to LIFO
+  * @stack: NULL
+  * @line_number: 0
+  * Return: nothing, void
+  */
+void lifo(stack_t **stack, size_t line_number)
+{
+	(void) stack;
+	(void) line_number;
+
+	element->IFO = 0;
+}
